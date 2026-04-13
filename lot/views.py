7a158af7c43dfd326e550188a770b59aae97e1a1
cayo <cayo@puigdefabregas.eu>
2026-04-13 10:22:02 +0200

    better managed delete lots with out lots

--- lot/views.py
+++ lot/views.py
@@ -117,14 +117,15 @@ class DeleteLotsView(LotSuccessUrlMixin, DashboardView, TemplateView ):
 
     def get(self, request, *args, **kwargs):
         selected_ids = request.GET.getlist('select')
-        if not selected_ids:
-            messages.error(request, _("No lots selected for deletion."))
-            return redirect(self.success_url)
         # check ownership
         lots_to_delete = Lot.objects.filter(
             id__in=selected_ids,
             owner=request.user.institution
         )
+        if not lots_to_delete.first():
+            messages.error(request, _("No lots selected for deletion."))
+            return redirect(self.success_url)
+
         context = {
             'lots': lots_to_delete,
             'lots_with_devices': any(lot.devices.exists() for lot in lots_to_delete),
@@ -136,14 +137,14 @@ class DeleteLotsView(LotSuccessUrlMixin, DashboardView, TemplateView ):
 
     def post(self, request, *args, **kwargs):
         selected_ids = request.POST.getlist('selected_ids')
-        if not selected_ids:
-            messages.error(request, _("No lots selected for deletion."))
-            return redirect(self.success_url)
-
         lots_to_delete = Lot.objects.filter(
             id__in=selected_ids,
             owner=request.user.institution
         )
+
+        if not lots_to_delete.first():
+            messages.error(request, _("No lots selected for deletion."))
+            return redirect(self.success_url)
 
         lot_tag = lots_to_delete.first().type
         deleted_count = lots_to_delete.delete()
